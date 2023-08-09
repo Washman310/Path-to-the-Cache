916d0cf86c6df0d5342efc243472be142ec4b4f7
Washman310 <134726876+Washman310@users.noreply.github.com>
2023-08-09 10:20:27 -0400

    Update index2.js
    Got rid of nextPage, changed returns back to true/false

--- index2.js
+++ index2.js
@@ -3,13 +3,11 @@ var password = "VW8123";
 function passcheck() {
     if(document.getElementById('pass1').value != password) {
         alert('Wrong Password');
-        var nextPage = "passfail.html"
-        return nextPage;
+        return false;
     }
 
     else{
         alert('Correct Password');
-        var nextPage = "geocache.html"
-        return nextPage;
+        return true;
     }
 }
