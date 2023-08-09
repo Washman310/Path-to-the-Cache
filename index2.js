8abd5625dbf74391bbb16fbac52134082e1ac5a3
Washman310 <134726876+Washman310@users.noreply.github.com>
2023-08-09 09:39:34 -0400

    Update index2.js
    change Pass1 in getElementById to password

--- index2.js
+++ index2.js
@@ -1,12 +1,12 @@
 var password = "VW8123";
 
 function passcheck() {
-    if(document.getElementById('Pass1').value != password) {
+    if(document.getElementById('password').value != password) {
         alert('Wrong Password');
         return false;
     }
 
-    if(document.getElementById('pass1').value == password) {
+    if(document.getElementById('password').value == password) {
         alert('Corect Password');
     }
 }
